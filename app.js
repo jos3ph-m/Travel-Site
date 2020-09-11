4ce457b28c9329e6904e955e8774d5d3de57b407
Joseph M <jmedina1985@hotmail.com>
2020-09-10 23:10:05 -0700

    More cursor animation, and title swipes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let controller;
 let slideScene;
 let pageScene;
+let mouse = document.querySelector(".cursor");
+let mouseTxt = mouse.querySelector("span");
 
 function animateSlides() {
   // Init Controller
@@ -59,11 +61,29 @@ function animateSlides() {
 }
 
 function cursor(e) {
-  let mouse = document.querySelector(".cursor");
   mouse.style.top = e.pageY + "px";
   mouse.style.left = e.pageX + "px";
 }
 
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, { y: "0%" });
+    mouseTxt.innerText = "Tap";
+  } else {
+    mouse.classList.remove("explore-active");
+    gsap.to(".title-swipe", 1, { y: "100%" });
+    mouseTxt.innerText = "";
+  }
+}
+
 window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
 
 animateSlides();
